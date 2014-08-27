bebdb7fce8f92673e929970236dd8c8b95bb9b45
Chase Menna <cbm318@lehigh.edu>
2014-08-27 19:01:07 +0000

    finished hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+// Chase Menna
+// hw01
+
+// name a class
+public class WelcomeClass {
+    
+    //name method
+    public static void main (String[] args) {
+        
+        System.out.println("    -----------     ");
+        System.out.println("    | Welcome |     "); 
+        System.out.println("    -----------     ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-C--B--M--3--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("I visit Cape Cod every summer   ");
+        System.out.println("and have been going there my whole life");
+        
+    }
+}
